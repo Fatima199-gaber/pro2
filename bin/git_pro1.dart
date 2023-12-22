823bf32a670817934b2f2b93adccd8a5c078f6a8
unknown <Fatima199-gaber@gmail.com>
2023-12-22 09:40:35 +0200

    first commit

---
+++ bin/git_pro1.dart
@@ -0,0 +1,5 @@
+import 'dart';
+
+void main() {
+
+}
---
+++ test/git_pro1_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_pro1/git_pro1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
